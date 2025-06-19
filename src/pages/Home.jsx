55f8f56f3cc1ca89c5e98441674bd2abd8fac59b
Arun Kumar <arunku9050@gmail.com>
2025-06-19 11:06:22 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/hero'
+import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffer from '../components/ExclusiveOffer'
 import Testimonial from '../components/Testimonial';
